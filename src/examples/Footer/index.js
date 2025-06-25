75b43bf18408f1719c1db691f376ec331955c618
davidbrodaa <david.broda@gmail.com>
2025-06-24 22:55:37 -0500

    Remove copyright comments from all files

--- src/examples/Footer/index.js
+++ src/examples/Footer/index.js
@@ -1,19 +1,4 @@
-/**
-=========================================================
-* Soft UI Dashboard PRO React - v4.0.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-pro-react
-* Copyright 2024 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// prop-types is a library for typechecking of props
+// prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
 
 // @mui material components
