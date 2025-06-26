2a58c536f2d64b2895a93857ea1e3f4e28cdf155
davidbrodaa <david.broda@gmail.com>
2025-06-26 18:02:41 -0500

    Updated index.js

--- src/layouts/pages/profile/components/Header/index.js
+++ src/layouts/pages/profile/components/Header/index.js
@@ -128,7 +128,7 @@ function Header() {
                 onChange={handleSetTabValue}
                 sx={{ background: "transparent" }}
               >
-               
+                <Tab label="App" icon={<Cube />} />
                 <Tab label="Message" icon={<Document />} />
                 <Tab label="Settings" icon={<Settings />} />
               </Tabs>
